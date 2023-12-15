5ecf4001084d4c78bf2e74f7fb19268580251b75
Andres Perez <andresperez2098@gmail.com>
2023-12-15 17:45:14 -0600

    Second commit. Update functionality

--- src/com/generation/Codigo4.java
+++ src/com/generation/Codigo4.java
@@ -3,13 +3,15 @@
  * para leer la entrada del usuario. Además se le agregó como argumento
  * System.in al Scanner para recibir el String del usuario. Se elminó
  * el scanner de uno de los jugadores, no era necesario.
+ * En la cadena de texto que recibe "scan" se le agregó el método ".toLowerCase()"
+ * para que convierta siempre la cadena a minúsculas y se realicen las comparaciones
+ * correctamente en cualquier caso.
  * Se agregó el método main para poder ejecutar el programa.
  * Se modificó la forma en que se compara el registro del jugador uno y dos.
  * Para esto se utilizaban los caracteres "==", lo que no es correcto
- * en Java, lo correcto es utilizar el método ".equals" o, en este caso,
- * ".equalsIgnoreCase" para comparar la cadena sin considerar las mayúsculas
- * o minúsculas.
+ * en Java, lo correcto es utilizar el método ".equals".
  * Se agregaron algunos "break" necesarios en los cases del switch utilizados.
+ * Se agregó el método ".close()" al scanner para terminar el proceso al final de main.
  */
 
 
@@ -24,28 +26,28 @@ public class Codigo4 {
         Scanner scan = new Scanner(System.in);
         
         System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-        String j1 = scan.nextLine();
+        String j1 = scan.nextLine().toLowerCase();
         
         System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
-        String j2 = scan.nextLine();
+        String j2 = scan.nextLine().toLowerCase();
 
-        if (j1.equalsIgnoreCase(j2)) {
+        if (j1.equals(j2)) {
             System.out.println("Empate");
         } else {
             int g = 2;
             switch (j1) {
                 case "piedra":
-                    if (j2.equalsIgnoreCase("tijeras")) {
+                    if (j2.equals("tijeras")) {
                         g = 1;
                     }
                     break;
                 case "papel":
-                    if (j2.equalsIgnoreCase("piedra")) {
+                    if (j2.equals("piedra")) {
                         g = 1;
                     }
                     break;
                 case "tijeras":
-                    if (j2.equalsIgnoreCase("papel")) {
+                    if (j2.equals("papel")) {
                         g = 1;
                     }
                     break;
